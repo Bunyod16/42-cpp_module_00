602838697042ffc7d96f0aa94f8ce1d0a37a57a5
Bunyod16 <bunyodshams@gmail.com>
2022-11-09 17:37:42 +0800

    init

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+int main(int ac, char **argv) {
+    int i;
+
+    i = 0;
+    if (ac == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    } else {
+        while (++i < ac) {
+            for (int j = 0; static_cast<size_t>(j) < strlen(argv[i]); j++) {
+                std::cout << static_cast<char>(toupper(argv[i][j]));
+            }
+        }
+    }
+    std::cout << "\n";
+}
